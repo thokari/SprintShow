96b18a4555e488af7e3b6f3da24627ae11de3785
HgSlv <h.silva@epages.com>
2017-08-11 17:33:23 +0200

    Adapt presentation to not use the API

---
+++ scripts/jiratasks.js
@@ -0,0 +1,58 @@
+define(function () {
+	var jiraTasks = [
+	{
+		id:"UNITY-3422",
+		name:"REST-API: Add product attribute (page) title",
+		type:"Technical Story",
+		summary:"For SEO reasons there should be an attribute called 'title' on the product resource which is mapped to the ep6 attribute 'DisplayTitle' and contains the information about the page title for the product page.",
+		priority:"Critical",
+		integrated:"7.11.0",
+		platforms:["ePagesJ"]
+	},
+	{
+		id:"AD-8038",
+		name:"REST API: Change locale of the Basket",
+		type:"New Feature",
+		summary:"We want to have a REST call to change the locale of the basket.",
+		priority:"Major",
+		integrated:"7.12.0",
+		platforms:["ePagesJ", "ePages6"]
+	},
+	{
+		id:"AD-7707",
+		name:"Change Mastercard Logo",
+		type:"Task",
+		summary:"As a merchant I do not want to show outdated payment logos to my customers.",
+		priority:"Major",
+		integrated:"7.11.0",
+		platforms:["ePages6"]
+	},
+	{
+		id:"AD-7402",
+		name:"Paypal Express: Redirect to Paypal automatically",
+		type:"Task",
+		summary:"On the 'pay with Paypal' step of the checkout we are now redirecting the endcustomer to Paypal automatically.",
+		priority:"Major",
+		integrated:"7.10.0",
+		platforms:["ePages6"]
+	},
+	{
+		id:"AD-7360",
+		name:"Paypal Express: Clean up settings page",
+		type:"Task",
+		summary:"As a merchant I dont want to be confused by to many options and links while trying to setting up Paypal",
+		priority:"Major",
+		integrated:"7.10.0",
+		platforms:["ePages6"]
+	},
+	{
+		id:"AD-7710",
+		name:"Research SSO for CM4all",
+		type:"Research",
+		summary:"Clarify the requirements and possible ways to solve",
+		priority:"Major",
+		integrated:"",
+		platforms:["ePagesJ", "ePages6"]
+	}
+	];
+};
--- scripts/sprintdata.js
+++ scripts/sprintdata.js
@@ -2,122 +2,122 @@ define(	function () {
 	return {
 		getCurrentSprint: function () {
 			return [
-				{
-					title: "PayPal Plus",
-					storyPoints : 35
-				},
-				{
-					title: "Paypal Express",
-					storyPoints : 6
-				},
-				{
-					title: "Avatax",
-					storyPoints : 0
-				},
-				{
-					title: "EasyMarketing",
-					storyPoints : 0
-				},
+			{
+				title: "PayPal Plus",
+				storyPoints : 35
+			},
+			{
+				title: "Paypal Express",
+				storyPoints : 6
+			},
+			{
+				title: "Avatax",
+				storyPoints : 0
+			},
+			{
+				title: "EasyMarketing",
+				storyPoints : 0
+			},
 			];
 		},
 		getCurrentSprintTypes: function () {
 			return [
-				{
-					title: "Bug",
-					storyPoints : 3
-				},
-				{
-					title: "Task",
-					storyPoints : 16
-				},
-				{
-					title: "Story",
-					storyPoints : 16
-				},
-				{
-					title: "Improvment",
-					storyPoints : 3
-				},
-				{
-					title: "Reseach",
-					storyPoints : 3
-				}
+			{
+				title: "Bug",
+				storyPoints : 3
+			},
+			{
+				title: "Task",
+				storyPoints : 16
+			},
+			{
+				title: "Story",
+				storyPoints : 16
+			},
+			{
+				title: "Improvment",
+				storyPoints : 3
+			},
+			{
+				title: "Reseach",
+				storyPoints : 3
+			}
 			];
 		},
 		getSprintHistory: function (){
 			return [
-				{
-					sprintName: "Sprint 1",
-								storyPoints: {
-									promised: "39",
-									leftOvers: "31",
-									pulled: "5"
-								}
-				},
-				{
-					sprintName: "Sprint 2",
-								storyPoints: {
-									promised: "49",
-									leftOvers: "16",
-									pulled: "0"
-								}
-				},
-				{
-					sprintName: "Sprint 3",
-								storyPoints: {
-									promised: "35",
-									leftOvers: "26",
-									pulled: "13"
-								}
-				},
-				{
-					sprintName: "Sprint 4",
-								storyPoints: {
-									promised: "32",
-									leftOvers: "0",
-									pulled: "0"
-								}
-				},
-				{
-					sprintName: "Sprint 5",
-								storyPoints: {
-									promised: "35",
-									leftOvers: "0",
-									pulled: "0"
-								}
-				},
-				{
-					sprintName: "Sprint 6",
-								storyPoints: {
-									promised: "29",
-									leftOvers: "5",
-									pulled: "0"
-								}
-				},
-				{
-					sprintName: "Sprint 7",
-								storyPoints: {
-									promised: "24",
-									leftOvers: "5",
-									pulled: "13"
-								}
-				},
-				{
-					sprintName: "Sprint 8",
-								storyPoints: {
-									promised: "30",
-									leftOvers: "8",
-									pulled: "9"
-								}
-				},
-				{
-					sprintName: "Sprint 9",
-								storyPoints: {
-									promised: "41",
-									leftOvers: "24",
-									pulled: "0"
-								}
+			{
+				sprintName: "Sprint 1",
+				storyPoints: {
+					promised: "39",
+					leftOvers: "31",
+					pulled: "5"
 				}
+			},
+			{
+				sprintName: "Sprint 2",
+				storyPoints: {
+					promised: "49",
+					leftOvers: "16",
+					pulled: "0"
+				}
+			},
+			{
+				sprintName: "Sprint 3",
+				storyPoints: {
+					promised: "35",
+					leftOvers: "26",
+					pulled: "13"
+				}
+			},
+			{
+				sprintName: "Sprint 4",
+				storyPoints: {
+					promised: "32",
+					leftOvers: "0",
+					pulled: "0"
+				}
+			},
+			{
+				sprintName: "Sprint 5",
+				storyPoints: {
+					promised: "35",
+					leftOvers: "0",
+					pulled: "0"
+				}
+			},
+			{
+				sprintName: "Sprint 6",
+				storyPoints: {
+					promised: "29",
+					leftOvers: "5",
+					pulled: "0"
+				}
+			},
+			{
+				sprintName: "Sprint 7",
+				storyPoints: {
+					promised: "24",
+					leftOvers: "5",
+					pulled: "13"
+				}
+			},
+			{
+				sprintName: "Sprint 8",
+				storyPoints: {
+					promised: "30",
+					leftOvers: "8",
+					pulled: "9"
+				}
+			},
+			{
+				sprintName: "Sprint 9",
+				storyPoints: {
+					promised: "41",
+					leftOvers: "24",
+					pulled: "0"
+				}
+			}
 
 			];
 		}
