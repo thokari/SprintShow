36fb45d6a283ddc12e731d3bcc8c51b8ee0337ae
HgSlv <h.silva@epages.com>
2017-09-27 12:07:41 +0200

    Add Jira Issues for Review 3

--- scripts/jiratasks.js
+++ scripts/jiratasks.js
@@ -1,12 +1,12 @@
 define(function () {
 	return [
 	{
-		id:"AD-8263",
-		name:"Trusted Shops: Change Logo",
+		id:"AD-8349",
+		name:"Appstore: Open installation links in a new tab",
 		type:"Task",
-		summary:"We need to change the Logo of the trusted shops integration in epages now and 6. The logo is shown on the settings page.",
+		summary:"We are linking to the external partner´s landing pages from the app consecution page in a new tab.",
 		priority:"Blocker",
-		integrated:"7.14.0",
+		integrated:"7.15.0",
 		platform:"ePages6",
 		links: [
 			"<a href='https://ocean-unity.epages.systems/epages/DemoShop.admin/sec59cb0ac59d/?ViewAction=MBO-ViewTrustedShopsCertificates&ObjectID=20878' target='_blank'>ePages6 MBO</a>",
@@ -14,25 +14,30 @@ define(function () {
 		]
 	},
 	{
-		id:"AD-8248",
-		name:"Paypal Plus: Improve wording on settings page",
-		type:"Task",
-		summary:"With the new payment selection page we are replacing the marketing text, so we can clean up the settings page of Paypal Plus",
+		id:"AD-8305",
+		name:"SOFORT: callbacks are not working in epages now",
+		type:"Bug",
+		summary:"",
 		priority:"Blocker",
-		integrated:"7.14.0",
-		platform:"ePages6",
-		links: [
-			"<a href='https://ocean-unity.epages.systems/epages/DemoShop.admin/sec59cb0ac59d/?ViewAction=MBO-ViewSettings&ObjectID=20942' target='_blank'>ePages6 MBO</a>",
-			"<a href='https://demounity.ocean-unity.epages.systems/epages/ukunity.admin/seccd1699821c/?ViewAction=MBO-ViewSettings&ObjectID=28491' target='_blank'>ePagesNow MBO</a>"
-		]
+		integrated:"7.15.0",
+		platform:"ePages6"
 	},
 	{
-		id:"AD-7756",
-		name:"Spreedly Stripe: Localize register Form",
-		type:"Improvement",
-		summary:"As a merchant I want to sign up for a new payment provider in my language, we were not setting a locale for the setup form of Stripe.",
+		id:"AD-8335",
+		name:"Amazon Pay: capture via order status 'dispatched' does not work anymore",
+		type:"Bug",
+		summary:"",
+		priority:"Blocker",
+		integrated:"7.15.0",
+		platform:"ePages6"
+	},
+	{
+		id:"AD-8254",
+		name:"Google Shopping: Activate mexican product feed",
+		type:"Task",
+		summary:"As a merchant I want to list my products at google shopping, in mexican language.",
 		priority:"Critical",
-		integrated:"7.14.0",
+		integrated:"7.15.0",
 		platform:"ePages6",
 		links: [
 			"<a href='https://demounity.ocean-unity.epages.systems/epages/demounity.admin/secb771980d4a/?ViewAction=MBO-ViewSettings&ObjectID=28493' target='_blank'>ePagesNow DE</a>",
@@ -40,22 +45,26 @@ define(function () {
 		]
 	},
 	{
-		id:"AD-6843",
-		name:"DHL: Switch to GK2.2 API",
-		type:"Epic",
-		summary:"Based on the results of the research task we should switch to the new GK2.2 API.",
-		priority:"Major",
-		integrated:"7.14.0",
-		platform:"ePages6"
+		id:"AD-8016",
+		name:"Show Postfinance as a dedicated payment group",
+		type:"Story",
+		summary:"In epages 6 Ingenico and Postfinance are activated as one feature, but Postfinance is only working with swiss merchants.",
+		priority:"Critical",
+		integrated:"7.15.0",
+		platform:"ePages6",
+		links: [
+			"<a href='https://demounity.ocean-unity.epages.systems/epages/demounity.admin/secb771980d4a/?ViewAction=MBO-ViewSettings&ObjectID=28493' target='_blank'>ePagesNow DE</a>",
+			"<a href='https://ukunity.ocean-unity.epages.systems/epages/ukunity.admin/sec30f1650aa0/?ViewAction=MBO-ViewSettings&ObjectID=28494' target='_blank'>ePagesNow UK</a>"
+		]
 	},
 	{
-		id:"AD-6310",
-		name:"Rest API Product: energyLabelsString doesn't get cleared after the labels are removed.",
-		type:"Bug",
-		summary:"The attribute energyLabelsString doesn't get cleared after removing the energy label for the product.",
-		priority:"Major",
-		integrated:"7.14.0",
-		platform:"ePagesJ"
+		id:"AD-7578",
+		name:"Remove GLS",
+		type:"Task",
+		summary:"This feature has no active users. We should remove the code entirely.",
+		priority:"Minor",
+		integrated:"7.15.0",
+		platform:"ePages6"
 	}
 	];
 });
