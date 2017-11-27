b52df03938649c6a51ef46f4ae6ea6b2df2d8e1e
Christian Breitkreutz <cbreitkreutz@epages.com>
2017-11-27 16:57:22 +0100

    currentState sprint 6

--- scripts/jiratasks.js
+++ scripts/jiratasks.js
@@ -1,83 +1,55 @@
 define(function () {
     return [
     {
-        "id": "UNITY-4149",
-        "name": "DHL Intraship: Not possible to input postnumber in epages now checkout",
-        "type": "Task",
-        "summary": "",
-        "priority": "Critical",
-        "integrated": "7.17.0",
-        "platform": "ePages6",
-        "links": [
-            "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3ADHLIntraship' target='_blank'>coverage</a>",
-            "<a href='https://demounity.teamred-06.vm-intern.epages.com' target='_blank'>SF</a>"
-        ]
-    },
-    {
-        "id": "AD-8411",
-        "name": "DHL Versenden: Remap address fields",
-        "type": "Bug",
+        "id": "EPG-30743",
+        "name": "Google Shopping: allow lower case letters in google product attributes",
+        "type": "Improvement",
         "summary": "",
         "priority": "Blocker",
-        "integrated": "7.17.0",
-        "platform": "ePages6",
-    },
-    {
-        "id": "AD-8402",
-        "name": "Idealo: Add 'Staffelung' / 'Increment' as a field to the feedfile",
-        "type": "Task",
-        "summary": "",
-        "priority": "Major",
-        "integrated": "7.17.0",
+        "integrated": "7.18.0",
         "platform": "ePages6",
         "links": [
-            "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3AIdealo' target='_blank'>coverage</a>",
-            "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3AIdealo' target='_blank'>MBO</a>"
+             "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3AGoogleBase' target='_blank'>coverage</a>",
+             "Postman"
         ]
     },
     {
-        "id": "AD-8279",
-        "name": "RemoteSearch: Use trigger cartridge for Category CRUD in order to update cache",
-        "type": "Task",
+        "id": "AD-8414",
+        "name": "API Callback URL for Apps only using the english default",
+        "type": "Bug",
         "summary": "",
-        "priority": "Major",
-        "integrated": "7.17.0",
+        "priority": "Critical",
+        "integrated": "7.18.0",
         "platform": "ePages6",
         "links": [
-            "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3ARemoteSearch' target='_blank'>coverage</a>",
-            "<a href='https://epages.atlassian.net/wiki/spaces/TI/pages/183353634/RemoteSearch+solr' target='_blank'>confluence (RemoteSearch)</a>",
-            "<a href='https://epages.atlassian.net/wiki/spaces/TI/pages/183310167/Trigger+Cartridge' target='_blank'>confluence (TriggerCartridge)</a>",
-            "<a href='https://github.com/ePages-de/Cartridges/blob/develop/DE_EPAGES/RemoteSearch/Data/SharedConfig/epages.conf' target='_blank'>epages.conf (RemoteSearch)</a>",
-            "<a href='https://github.com/ePages-de/Cartridges/blob/develop/DE_EPAGES/Trigger/Data/SharedConfig/epages.conf' target='_blank'>epages.conf(TriggerCartridge)</a>"
+             "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3AAppStore' target='_blank'>coverage</a>",
+             "Link to MBO"
         ]
     },
     {
-        "id": "AD-8397",
-        "name": "RestAPI: can't handle deleted coupon campaigns",
-        "type": "Task",
+        "id": "AD-8369",
+        "name": "Rest API: Add order documents to the 'order endpoint'",
+        "type": "Story",
         "summary": "",
         "priority": "Critical",
         "integrated": "7.17.0",
         "platform": "ePages6",
         "links": [
-            "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3AOrder' target='_blank'>coverage</a>",
-            "<a href='https://teamred-06.vm-intern.epages.com/epages/DemoShop.admin/secb36e790b59/?ViewAction=MBO-ViewCouponCampaigns&ObjectID=20490' target='_blank'>Coupon campaigns</a>",
-            "postman"
+             "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3AOrder' target='_blank'>coverage</a>",
+             "Link to MBO"
         ]
     },
     {
-        "id": "AD-8296",
-        "name": "Paypal Plus: Simplified logging of the onboarding process",
+        "id": "AD-8336",
+        "name": "Add ESF Tests to our pipeline",
         "type": "Task",
         "summary": "",
-        "priority": "Critical",
-        "integrated": "7.17.0",
+        "priority": "Major",
+        "integrated": "7.18.0",
         "platform": "ePages6",
         "links": [
-            "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3APayPalPlus' target='_blank'>coverage</a>",
-            "<a href='https://teamred-06.vm-intern.epages.com/epages/Site.admin?ObjectPath=/Providers/Distributor&ViewAction=PBO-ViewPBO&ChangeAction=SaveLoginForm&Login=admin&Password=admin' target='_blank'>PBO</a>"
+            
         ]
     }
-    
 ]
 })
