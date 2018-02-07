6116382535d6bdb60ee52399ac1f5785976fab41
HgSlv <h.silva@epages.com>
2018-02-07 18:37:06 +0100

    Sprint 9

--- scripts/jiratasks.js
+++ scripts/jiratasks.js
@@ -1,36 +1,36 @@
 define(function () {
     return [
     {
-        "id": "AD-8515",
-        "name": "Clean up REST-API-Tests",
-        "type": "Improvement",
+        "id": "AD-8509",
+        "name": "DHL Versenden: truncate all fields according to the WSDL file",
+        "type": "Bug",
         "summary": "",
-        "priority": "Blocker",
-        "integrated": "7.20.0",
-        "platform": "ePagesj",
+        "priority": "Critical",
+        "integrated": "7.21.0",
+        "platform": "ePages6",
         "links": [
         ]
     },
     {
-        "id": "AD-8398",
-        "name": "Skrill: Add Astropay",
+        "id": "AD-8564",
+        "name": "DHL: Use new pricelist",
         "type": "Task",
         "summary": "",
-        "priority": "Critical",
-        "integrated": "7.20.0",
+        "priority": "Major",
+        "integrated": "7.21.0",
         "platform": "ePages6",
         "links": [
              "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3AMoneybookers' target='_blank'>coverage</a>"
         ]
     },
     {
-        "id": "AD-8511",
-        "name": "Skrill: Adjust pricing shown to the merchant",
-        "type": "Task",
+        "id": "AD-8585",
+        "name": "Epages-ui not appearing in epagesj access logs",
+        "type": "Bug",
         "summary": "",
         "priority": "Major",
-        "integrated": "7.20.0",
-        "platform": "ePages6",
+        "integrated": "7.21.0",
+        "platform": "ePagesj",
         "links": [
              "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3AMoneybookers' target='_blank'>coverage</a>",
              "<a href='https://epages.atlassian.net/secure/attachment/90488/Former_Skrill_pricing_in_paymentGroup.png' target='_blank'>pricing before</a>",
@@ -38,12 +38,12 @@ define(function () {
         ]
     },
     {
-        "id": "AD-7959",
-        "name": "WorldPay Link not working",
+        "id": "AD-8524",
+        "name": "PayPalPlus: onboarding disconnect button",
         "type": "Task",
         "summary": "",
         "priority": "Major",
-        "integrated": "7.20.0",
+        "integrated": "7.21.0",
         "platform": "ePages6",
         "links": [
             "<a href='https://barcelona.epages.systems/epages/DemoShop.admin/seca14198e949/?ViewAction=MBO-ViewSettings&ObjectID=40683' target='_blank'>MBO ePages6</a>",
@@ -51,12 +51,54 @@ define(function () {
         ]
     },
     {
-        "id": "AD-8485",
-        "name": "Confianza: Change logo",
+        "id": "AD-8587",
+        "name": "Clarify test and documentation for epagesj image upload",
         "type": "Task",
         "summary": "",
         "priority": "Major",
-        "integrated": "7.20.0",
+        "integrated": "7.21.0",
+        "platform": "ePagesj",
+        "links": [
+            "<a href='img/former_mbo_confianza.png' target='_blank'>MBO Before</a>",
+            "<a href='https://barcelona.epages.systems/epages/DemoShop.admin/seca14198e949/?ViewAction=MBO-ViewConfianzaOnline&ObjectID=20603' target='_blank'>MBO After</a>",
+            "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3AConfianzaOnline' target='_blank'>coverage</a>",
+        ]
+    },
+    {
+        "id": "AD-8598",
+        "name": "Stabilize the Shop Wizard in ESF tests",
+        "type": "Bug",
+        "summary": "",
+        "priority": "Blocker",
+        "integrated": "7.21.0",
+        "platform": "ePages6",
+        "links": [
+            "<a href='img/former_mbo_confianza.png' target='_blank'>MBO Before</a>",
+            "<a href='https://barcelona.epages.systems/epages/DemoShop.admin/seca14198e949/?ViewAction=MBO-ViewConfianzaOnline&ObjectID=20603' target='_blank'>MBO After</a>",
+            "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3AConfianzaOnline' target='_blank'>coverage</a>",
+        ]
+    },
+    {
+        "id": "AD-8604",
+        "name": "Product creation search delay increased in REST API tests",
+        "type": "Bug",
+        "summary": "",
+        "priority": "Major",
+        "integrated": "7.21.0",
+        "platform": "ePagesj",
+        "links": [
+            "<a href='img/former_mbo_confianza.png' target='_blank'>MBO Before</a>",
+            "<a href='https://barcelona.epages.systems/epages/DemoShop.admin/seca14198e949/?ViewAction=MBO-ViewConfianzaOnline&ObjectID=20603' target='_blank'>MBO After</a>",
+            "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3AConfianzaOnline' target='_blank'>coverage</a>",
+        ]
+    },
+    {
+        "id": "AD-8606",
+        "name": "Loggin General context",
+        "type": "Improvement",
+        "summary": "",
+        "priority": "Major",
+        "integrated": "7.21.0",
         "platform": "ePages6",
         "links": [
             "<a href='img/former_mbo_confianza.png' target='_blank'>MBO Before</a>",
